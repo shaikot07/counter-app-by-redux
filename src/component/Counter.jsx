34aa8730075b7634e028fc804d4934fa7c1d01ef
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-30 00:13:14 +0600

    added onclick function for increment decrement and reset and use redux and react redux

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { incrementCounter } from '../assets/redux-element/actions/CounterActions';
+import { decrementCounter, incrementCounter, resetCounter } from '../assets/redux-element/actions/CounterActions';
 
 const Counter = () => {
       const count = useSelector((state) => state.count);
@@ -9,13 +9,27 @@ const Counter = () => {
       const handleIncrement=()=>{
             dispatch(incrementCounter())
       }
+      const handleDecrement=()=>{
+            dispatch(decrementCounter())
+      }
+      const handleResetCounter=()=>{
+            dispatch(resetCounter())
+      }
       return (
             <div className='text-center'>
                   <h2 className='text-center font-semibold text-2xl'>Counter App</h2>
                   <h2 className=' font-semibold text-1xl'>Count : {count}</h2>
+                  <div>
                   <button onClick={handleIncrement} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700">
-                        Primary Button
+                        Increment
                   </button>
+                  <button onClick={handleDecrement} className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-700">
+                        Decrement
+                  </button>
+                  <button onClick={handleResetCounter} className="bg-pink-700 text-white px-4 py-2 rounded-md hover:bg-pink-500">
+                        reset
+                  </button>
+                  </div>
 
             </div>
       );
