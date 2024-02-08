bc996378db148102a0c128ddb7ad18a209228a80
shaikot07 <saifulislamshaikat007@gmail.com>
2024-02-08 23:58:29 +0600

    setup full from

--- src/component/checkoutFrom/CheckOutFrom.jsx
+++ src/component/checkoutFrom/CheckOutFrom.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const CheckOutFrom = () => {
+       const handleSaveAddress= (event)=>{
+            event.preventDefault()
+            const form = event.target
+            const fullName = form.fullName.value;
+            const shippingAddress = form.shippingAddress.value;
+            const mobileNumber=form.mobileNumber.value;
+            const billingAddress= form.billingAddress.value;
+            const paymentAddressInfo={fullName, shippingAddress,mobileNumber,billingAddress,};
+            console.log(paymentAddressInfo);
+       }
       return (
-            <div className='p-2 w-[600px] bg-emerald-500 ml-10'>
+            <div className='p-2 w-[320px] bg-emerald-500 ml-10'>
                   <h3 className='text-2xl'>Add New Delivery Address</h3>
-                  <form>
-                        <div className='grid grid-cols-1 md:grid-cols-2 gap-2 '> 
+                  <form onSubmit={ handleSaveAddress}>
+                        <div className='grid grid-cols-1 gap-2 '> 
+                              {/* for full name  data*/}
                               <div>
                               <label className="block text-md  text-gray-700">Full Name</label>
                               <input type="text"
@@ -15,15 +26,7 @@ const CheckOutFrom = () => {
                                     className="appearance-none border border-primary rounded-md py-2 px-4 w-full max-w-xs text-gray-700 leading-tight focus:outline-none focus:border-primary focus:ring-primary"
                               />
                               </div>
-                              <div>
-                              <label className="block text-md  text-gray-700">Address</label>
-                              <input type="text"
-                                    placeholder="Input Your Address"
-                                    required
-                                    name='address'
-                                    className="appearance-none border border-primary rounded-md py-2 px-4 w-full max-w-xs text-gray-700 leading-tight focus:outline-none focus:border-primary focus:ring-primary"
-                              />
-                              </div>
+                              {/* for mobile data */}
                               <div>
                               <label className="block text-md  text-gray-700">Mobile Number</label>
                               <input type="text"
@@ -33,20 +36,31 @@ const CheckOutFrom = () => {
                                     className="appearance-none border border-primary rounded-md py-2 px-4 w-full max-w-xs text-gray-700 leading-tight focus:outline-none focus:border-primary focus:ring-primary"
                               />
                               </div>
+                                 {/* for shipping address data */}
                               <div>
-                              <label className="block text-md  text-gray-700">City</label>
+                              <label className="block text-md  text-gray-700">Shipping Address</label>
                               <input type="text"
-                                    placeholder="Your city name"
+                                    placeholder="Input Your Address"
                                     required
-                                    name='city'
+                                    name='shippingAddress'
                                     className="appearance-none border border-primary rounded-md py-2 px-4 w-full max-w-xs text-gray-700 leading-tight focus:outline-none focus:border-primary focus:ring-primary"
                               />
                               </div>
-                              
-
+                              {/* for billing address data  */}
+                              <div>
+                              <label className="block text-md  text-gray-700">Billing Address</label>
+                              <input type="text"
+                                    placeholder="Your Billing Address"
+                                    required
+                                    name='billingAddress'
+                                    className="appearance-none border border-primary rounded-md py-2 px-4 w-full max-w-xs text-gray-700 leading-tight focus:outline-none focus:border-primary focus:ring-primary"
+                              />
+                              </div>
+                               
 
                         </div>
-                        <button type='submit' className='bg-black text-white py-2 px-4 mt-2'>Save</button>
+                        {/* from submit button here  */}
+                        <button type='submit' className='bg-black text-white py-2 px-4 mt-2 rounded-md hover:bg-[#F85455]'>Save</button>
 
                   </form>
             </div>
